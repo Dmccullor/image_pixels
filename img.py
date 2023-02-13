63700d20e2cac7086ff50f44e9856def9b665824
Dylan McCullor <dmccullor0@gmail.com>
2023-02-12 19:11:05 -0600

    first commit

---
+++ img.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+
+cwd = (f'{os.getcwd()}/images')
+file_to_save = os.path.join('output.txt')
+data = []
+index = 0
+max = 0
+
+for i in os.scandir(cwd):
+    img = Image.open(i.path)
+    filepath = img.filename.split('\\')
+    filename = filepath[-1]
+    data.insert(index, [filename, img.height, img.width])
+
+    if len(filename) > max:
+        max = len(filename)
+
+    index += 1
+    
+headers = "|" + "Filename" + (" " * (max - 8)) + "| Height| Width|\n" + ("-" * (max + 17)) + "\n"
+print(headers)
+
+index = 0
+
+with open(file_to_save, 'w') as txt_file:
+    txt_file.write(headers)
+    for entry in data:
+        string = "|" + str(data[index][0]) + (" " * (max - len(data[index][0]))) + "|" + (" " * (7 - len(str(data[index][1])))) + str(data[index][1]) + "|" + (" " * (6 - len(str(data[index][2])))) + str(data[index][2]) + "|\n"
+        print(string)
+        txt_file.write(string)
+        index +=1
